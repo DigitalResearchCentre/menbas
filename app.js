6c837a0239b344afb023d1e495982b2e7a186d6c
Xiaohan Zhang <xiaohan.zhang@arts.usask.ca>
2016-03-01 17:50:54 -0600

    tmp

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
 var express = require('express')
   , session = require('express-session')
+  , MongoStore = require('connect-mongo')(session)
   , path = require('path')
   , db = require('./db')
+  , favicon = require('serve-favicon')
+  , logger = require('morgan')
+  , cookieParser = require('cookie-parser')
+  , bodyParser = require('body-parser')
+  , passport = require('passport')
 ;
-
-db.connect('mongodb://localhost:27017/menbas');
-
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-
-
-
-var routes = require('./routes/index');
-
 var app = express();
-
-
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(logger('dev'));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-
 app.use(bodyParser.json({
   limit: '20mb',
 }));
@@ -47,48 +34,57 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({ secret: 'mynewrandsecretfoobar' }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use('/', routes);
+db.connect('mongodb://localhost:27017/menbas', function(err, _db) {
+  var routes = require('./routes/index');
 
-app.all('/app/*', function(req, res) {
-  res.status(200).set({
-    'content-type': 'text/html; charset=utf-8'
-  }).sendFile(path.join(__dirname, 'public/index.html'));
-});
+  app.use(session({
+    key: 'session',
+    secret: 'mynewrandsecretfoobar',
+    store: new MongoStore({db: _db}),
+    resave: false,
+    saveUninitialized: false,
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use('/', routes);
 
+  app.all('/app/*', function(req, res) {
+    res.status(200).set({
+      'content-type': 'text/html; charset=utf-8'
+    }).sendFile(path.join(__dirname, 'public/index.html'));
+  });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
-// error handlers
+  // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: {}
     });
   });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
-
 
 module.exports = app;
--- public/app/boot.js
+++ public/app/boot.js
@@ -6,12 +6,20 @@ require('jquery');
 require('bootstrap');
 require('./app.less');
 
+var redux = require('redux')
+  , thunk = require('redux-thunk')
+  , createStore = redux.createStore
+  , applyMiddleware = redux.applyMiddleware
+  , store = applyMiddleware(thunk)(createStore)()
+;
+
 var AppComponent = require('./app.component');
 
 document.addEventListener('DOMContentLoaded', function() {
   ng.platform.browser.bootstrap(AppComponent, [
     ng.http.HTTP_PROVIDERS,
     ng.router.ROUTER_PROVIDERS,
+    require('ng2-redux')(store),
     require('./services/api'),
   ]).catch(function(err) {
     console.error(err);
